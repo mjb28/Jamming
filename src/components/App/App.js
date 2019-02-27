4b47914ce575b4a1d9c1a27d4a1342f4f099cab8
mjb28 <mattjb28@gmail.com>
2019-02-27 22:55:20 +1100

    next release

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,14 +4,15 @@ import './App.css';
 import SearchBar from  '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist'
+import Spotify from '../../util/Spotify'
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = { 
-      SearchResults: ['name', 'artist', 'album', 'id'],
+      searchResults: [],
       playlistName: 'Playlist1',
-      playlistTracks: ['name', 'artist', 'album', 'id']
+      playlistTracks: []
   }
   this.addTrack = this.addTrack.bind(this);
   this.updatePlaylistName = this.updatePlaylistName.bind(this);
@@ -31,24 +32,33 @@ updatePlaylistName(name){
 
 savePlaylist(){
   let trackURIs = this.state.playlistTracks.map(track => track.uri)
+  Spotify.savePlaylist(this.state.playlistName, trackURIs);
+    this.setState({
+      searchResults: []
+    });
+    this.updatePlaylistName('My playlist');
+    console.info(trackURIs);
 }
 
 search(term){
-  console.log(term);
+  Spotify.search(term)
+      .then(results => this.setState({
+        searchResults: results
+      }));
 }
 
-  render() {
-    return (
-        <div>
-        <h1>Ja<span className="highlight">mmm</span>ing</h1>
-        <div className="App" />
-          <SearchBar search={this.state.search} />
-          <div className="App-playlist" />
-            <SearchResults searchresults={this.state.SearchResults} onAdd={this.addTrack} />
-            <Playlist playlist={this.state.playlistName} playlisttracks={this.state.playlistTracks} onRemove={this.state.onRemove} onNameChange={this.state.updatePlaylistName} onSave={this.state.savePlaylist} />
-      </div>
-    );
-  }
+render() {
+  return (
+      <div>
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+      <div className="App" />
+        <SearchBar search={this.state.search} />
+        <div className="App-playlist" />
+          <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
+          <Playlist playlist={this.state.playlistName} playlisttracks={this.state.playlistTracks} onRemove={this.onRemove} onNameChange={this.updatePlaylistName} onSave={this.savePlaylist} />
+    </div>
+  );
+}
 }
 
 export default App;
--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -15,7 +15,7 @@ class Playlist extends Component {
   render() {
     return (
         <div className="Playlist">
-        <input defaultValue={this.props.name} onChange={this.state.handleNameChange} />
+        <input defaultValue={this.props.name} onChange={this.handleNameChange} />
         <Tracklist playlistTracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval={true} />
         <a className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</a>
       </div>
